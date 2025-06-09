775db9f3568ab0e2c4a57dab3e2aa7bbdfa8259c
Danilo Silva <danilo.silva@msn.com>
2025-06-09 10:58:22 -0300

    Refactor Angular application structure by replacing AppComponent with a standalone component, updating the main entry point to use bootstrapModule, and removing unused HTML, SCSS, and configuration files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: '<router-outlet></router-outlet>',
+  styles: [
+    `
+      :host {
+        display: block;
+        height: 100%;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-  title = 'Teste';
-}
+export class AppComponent {}
