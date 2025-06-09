54d1bfb5b3fd424ed97e673a2a2988566a5291ff
Danilo Silva <danilo.silva@msn.com>
2025-06-09 11:48:58 -0300

    Refactor main layout by replacing static sidenav content with a reusable sidenav component, and remove unused SCSS styles for improved maintainability.

--- src/app/layout/containers/main-layout/main-layout.component.ts
+++ src/app/layout/containers/main-layout/main-layout.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
+import { SidenavComponent } from '../../components/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SidenavComponent],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
 })
