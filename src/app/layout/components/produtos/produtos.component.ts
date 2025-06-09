5b8e1e4761510ea18dbe2e18aadde427281c309f
Danilo Silva <danilo.silva@msn.com>
2025-06-09 12:12:50 -0300

    Refactor home component layout by replacing static elements with utility classes, enhancing styling consistency, and adding a new produtos component for product display.

---
+++ src/app/layout/components/produtos/produtos.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos',
+  standalone: true,
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.scss'],
+})
+export class ProdutosComponent {}
