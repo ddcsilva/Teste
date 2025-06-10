448abf11ac98231038b2b8ea057da73a9aad4284
Danilo Silva <danilo.silva@msn.com>
2025-06-09 22:08:02 -0300

    Remove unused styles from AppComponent to streamline the component structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
-  styles: [
-    `
-      :host {
-        display: block;
-        height: 100%;
-      }
-    `,
-  ],
 })
 export class AppComponent {}
